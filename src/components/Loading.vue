d380199b9ba4a58f9e941c9ff772581fce5effb4
tuchin1228 <1104108110@nkust.edu.tw>
2020-11-27 03:08:01 +0800

    reset components

--- src/components/Loading.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="LoadingBg">
-    <img src="@/assets/img/loading.gif" alt="Loading" />
-  </div>
-</template>
-
-<style scoped lang="scss">
-@import "@/assets/loading.scss";
-</style>
